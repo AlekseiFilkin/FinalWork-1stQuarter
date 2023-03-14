cf4a040b50d78daef36db7c1010e03e78b400ba7
Aleksey_Filkin <acid_wind@mail.ru>
2023-03-14 22:08:01 +0700

    FinalTask solved

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿/*Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
+string[] stringArray = { "Hello", "2", "world", ":-)" };
+
+string[] CreateArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    return newArray;
+}
+
+string[] FillAndCutArray(string[] array, string[] newArray)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    Array.Resize(ref newArray, j);
+    return newArray;
+}
+
+void PrintArray(string[] newArray)
+{
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write($"'{newArray[i]}'");
+    }
+}
+
+string[] resultArray = CreateArray(stringArray);
+resultArray = FillAndCutArray(stringArray, resultArray);
+Console.WriteLine("Массив из строк, длина которых меньше, либо равна 3 символам:");
+PrintArray(resultArray);
